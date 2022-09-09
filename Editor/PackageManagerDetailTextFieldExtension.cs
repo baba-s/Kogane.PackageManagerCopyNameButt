3fb4a217935b3e7061eb9ae91c12082d5ab8da64
baba-s <shotababa1206@gmail.com>
2022-09-09 19:55:47 +0900

    add

---
+++ Editor/PackageManagerDetailTextFieldExtension.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal sealed class PackageManagerDetailTextFieldExtension
+        : VisualElement,
+          IPackageManagerExtension
+    {
+        private TextField m_textField;
+
+        static PackageManagerDetailTextFieldExtension()
+        {
+            var extension = new PackageManagerDetailTextFieldExtension();
+            PackageManagerExtensions.RegisterExtension( extension );
+        }
+
+        VisualElement IPackageManagerExtension.CreateExtensionUI()
+        {
+            m_textField = new TextField
+            {
+                multiline        = true,
+                selectAllOnFocus = true,
+                style =
+                {
+                    height = 128,
+                },
+            };
+
+            return m_textField;
+        }
+
+        void IPackageManagerExtension.OnPackageSelectionChange( PackageInfo packageInfo )
+        {
+            var json      = JsonUtility.ToJson( packageInfo, true );
+            var isVisible = !string.IsNullOrWhiteSpace( json );
+
+            m_textField.visible = isVisible;
+
+            if ( !isVisible ) return;
+
+            m_textField.value = json;
+        }
+
+        void IPackageManagerExtension.OnPackageAddedOrUpdated( PackageInfo packageInfo )
+        {
+        }
+
+        void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.PackageManagerCopyNameButt
+# Kogane Package Manager Detail Text Field Extension
+
+パッケージの詳細を JSON で表示する Text Field を Package Manager に追加するエディタ拡張
